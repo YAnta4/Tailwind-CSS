590ce80d47f32bf6e050472ec9086fe55f472d5c
YAnta4 <youssefantar190@gmail.com>
2026-07-22 21:50:26 +0200

    First commet

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  // هنا بتحدد للمترجم فين ملفات الـ HTML بتاعتك عشان يراقبها
+  content: [
+    "./*.{html,js}",
+    "./src/**/*.{html,js}"
+  ],
+  theme: {
+    extend: {
+     
+    },
+  },
+  plugins: [],
+}
